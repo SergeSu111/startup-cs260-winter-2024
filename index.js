1fc21454ac42e7e7fea8ecf058a627ed8dda16a7
Junha <sjh666@byu.edu>
2024-03-27 10:46:16 -0600

    configue the json and static public and get the port and construct the express

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-express = require(express);
+const express = require(express); // get the express 
+const app = express(); // construct the express to app
+
+// get the serivce port 4000, 
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// use express to use json() to make the response and required body as json
+app.use(express.json());
+
+// static the front end conent
+app.use(express.static("public"));
+
+// Router for service endpoints 
+var apiRouter = express.Router();
+app.use("/api", apiRouter);
+
+// Specific endpoints
+app.post("/addingWebsite", (req, res) =>
+{
+    new
+}
+)
