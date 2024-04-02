9a7710a29ac3819ff3e74a777f71065402428aa2
Junha <sjh666@byu.edu>
2024-04-02 16:48:02 -0600

    finished the register front-end request

--- public/normal_js/login.js
+++ public/normal_js/login.js
@@ -1,13 +1,15 @@
 let my_button = document.querySelector("#my_button");
 
+let username = document.querySelector("#name").value; 
+let password = document.querySelector("#password").value; // 把密码拿过来
+
 async function log_in()
 {
-        let username = document.querySelector("#name").value; 
-        let password = document.querySelector("#password").value; // 把密码拿过来
+
         const response = await fetch('/login/:username', {  // fetch request to backend 
             method: 'POST',
             headers: {'content-type': 'application/json'},
-            body: JSON.stringify({"username": username, "password":password})
+            body: JSON.stringify({"username": username, "password":password}) // 因为body request 需要是一个object
         });
         if (response.ok)
         {
@@ -21,13 +23,20 @@ async function log_in()
     }
 
 
-
-// my_button.addEventListener("click", function(event){
-//     event.preventDefault();
-//     log_in();
-// });
-
 async function register()
 {
-    
+    const response = await fetch("/register/:username", {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({"username": username, "password": passowrd})
+    });
+
+    if (response.ok)
+    {
+        alert("You successfully registered your account.");
+    }
+    else
+    {
+        alert((await response.json().message));  // 否则的话返回后端返回的消息
+    }
 }
