16c9652b0e3404eec5ff43c96dcb530034f6f883
Junha <sjh666@byu.edu>
2024-04-02 16:42:31 -0600

    finished the log_in front_end fetch function

--- public/normal_js/login.js
+++ public/normal_js/login.js
@@ -1,11 +1,26 @@
 let my_button = document.querySelector("#my_button");
 
-function log_in()
+async function log_in()
 {
-    let username = document.querySelector("#name").value; 
-    localStorage.setItem("Username", username);
-    window.location.href = "home_page.html";
-}
+        let username = document.querySelector("#name").value; 
+        let password = document.querySelector("#password").value; // 把密码拿过来
+        const response = await fetch('/login/:username', {  // fetch request to backend 
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({"username": username, "password":password})
+        });
+        if (response.ok)
+        {
+            window.location.href = "home_page.html";
+        }
+        else
+        {
+        
+            alert((await response.json()).message); // 否则的话返回后端返回的消息
+        }
+    }
+
+
 
 // my_button.addEventListener("click", function(event){
 //     event.preventDefault();
