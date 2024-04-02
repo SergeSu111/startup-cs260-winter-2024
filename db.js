ab1bb5532db5f41a9d84b97065f6952c4a2f5cd3
Junha <sjh666@byu.edu>
2024-04-01 20:24:06 -0600

    configured the database, and it is working perfect

--- db.js
+++ db.js
@@ -1,1 +1,13 @@
 const config = require("./dbConfig.json"); // call the config
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`; //get the mongo db url,automatically generate infor for me
+const { MongoClient } = require('mongodb');// grab the MongoClient property in mongodb object 
+const client = new MongoClient(url); // get myown mongo db
+const db = client.db("rental"); //create a db name called rental
+
+(async function testConnection()
+{
+    await client.connect(); // wait for the db is connected
+    await db.command({ping:1});
+})().catch((ex) => {console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+});
+
