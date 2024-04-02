b9fceb5c2a6d5018a330c4ff967c4e0ffbe6c061
Junha <sjh666@byu.edu>
2024-04-01 20:49:50 -0600

    installed uuid and bcrypt and imported them. But the db is still not connected

--- db.js
+++ db.js
@@ -2,12 +2,24 @@ const config = require("./dbConfig.json"); // call the config
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`; //get the mongo db url,automatically generate infor for me
 const { MongoClient } = require('mongodb');// grab the MongoClient property in mongodb object 
 const client = new MongoClient(url); // get myown mongo db
-const db = client.db("rental"); //create a db name called rental
+
+const db = client.db("startup-CS260"); //create a db name called rental
+const userCollection = db.collection("user"); // 创造了叫user的collection
+const formCollection = db.collection("suggestion"); 
+const webCollection = db.collection("webInput"); 
+
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
+
 
 (async function testConnection()
 {
     await client.connect(); // wait for the db is connected
     await db.command({ping:1});
 })().catch((ex) => {console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+process.exit(1);
 });
+
+
+
 
